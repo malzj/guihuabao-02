7b190644a73ab225002d6989b704179f8af8a5bf
609658960@qq.com <609658960@qq.com>
2015-11-30 13:26:17 +0800

    1

--- grails-app/controllers/com/guihuabao/FrontController.groovy
+++ grails-app/controllers/com/guihuabao/FrontController.groovy
@@ -3698,16 +3698,16 @@ class FrontController {
     * 字符串拼接
     * 参数 arr (要转换成字符串的数组)
     * */
-    def strImplode(ArrayList arr){
-        StringBuilder str = new StringBuilder();
-        int offset = arr.size() - 1;
-        for( int i = 0; i < offset; i++ )
-        {
-            str.append(arr[i]).append("|||||");
-        }
-        str.append(arr[offset]);
-        return  str.toString();
-    }
+//    def strImplode(ArrayList arr){
+//        StringBuilder str = new StringBuilder();
+//        int offset = arr.size() - 1;
+//        for( int i = 0; i < offset; i++ )
+//        {
+//            str.append(arr[i]).append("|||||");
+//        }
+//        str.append(arr[offset]);
+//        return  str.toString();
+//    }
 
      /*
     * 测试结果
